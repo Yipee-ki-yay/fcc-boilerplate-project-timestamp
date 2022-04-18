0a1056188febc062e7008271236043c51515f2bf
Alexander Morozov <alexander.morozov@shijigroup.com>
2022-04-18 21:19:35 +0300

    implement Timestamp Microservice

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+const moment = require('moment')
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -30,3 +31,27 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get('/api/:unixParam([0-9]+)', (req, res) => {
+  const unixParam = Number(req.params.unixParam) / 1000
+  const unix = Number(req.params.unixParam)
+  const utc = moment.unix(unixParam).format('ddd, DD MMM YYYY HH:mm:ss')
+
+  res.json({
+    unix,
+    utc
+  })
+})
+
+app.get('/api/:date([0-9]{4}-[0-9]{2}-[0-9]{2})', (req, res) => {
+  console.log('date req.params.date', req.params.date);
+
+  const date = req.params.date
+  const unix = Number(moment(date).format('x'))
+  const utc = moment(date).format('ddd, DD MMM YYYY HH:mm:ss')
+
+  res.json({
+    unix,
+    utc
+  })
+})
